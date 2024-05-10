a23c421ebf11fe2c2605129b3e8071e716acd1b5
Mohit <92150824+MKG26@users.noreply.github.com>
2024-05-10 13:51:57 +0530

    Initial Commit

---
+++ Dyslexic Ally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dyslexic Ally
+//
+//  Created by Mohit Kumar Gupta on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
