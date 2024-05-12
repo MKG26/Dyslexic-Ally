38c8c8ad7d62d4bc3a5e1cc482257253da441f93
Mohit <92150824+MKG26@users.noreply.github.com>
2024-05-13 01:12:06 +0530

    .

--- Dyslexic Ally/SecondViewController.swift
+++ Dyslexic Ally/SecondViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 14.0
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "xyz5", ofType: "mp4")!))
+        
+        let layer = AVPlayerLayer(player: player)
+        
+        
+        layer.frame = CGRect(x: 0, y: 0, width: view.bounds.width / 2, height: view.bounds.height / 2)
+
+        layer.position.x = 283
+        layer.position.y = 573
+        
+        
+        
+        view.layer.addSublayer(layer)
+        
+        player.play()
     }
     
 
--- Dyslexic Ally/ViewController.swift
+++ Dyslexic Ally/ViewController.swift
@@ -6,16 +6,44 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var button: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 14.0
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "xyz4", ofType: "mp4")!))
+        
+        let layer = AVPlayerLayer(player: player)
+        
+        
+        layer.frame = CGRect(x: 0, y: 0, width: view.bounds.width / 2, height: view.bounds.height / 2)
+
+        layer.position.x = 283
+        layer.position.y = 573
+        
+        
+        
+        view.layer.addSublayer(layer)
+        
+        player.play()
+    }
+    
+    
+    
 
 }
 
